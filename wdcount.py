a6386af6e5e4c24e48840cbe1611910dd9511db9
Vaibhaw <kvaibhaw99@gmail.com>
2017-07-28 11:25:00 -0400

    Added WdCount.py

--- wdcount.py
+++ wdcount.py
@@ -7,16 +7,32 @@ import sys
 # Chech for command line arguments
 # If not available then exit
 
-
 if len(sys.argv) != 2:
         sys.exit("Usage: python wdcount.py test.txt")
 
+
+# Else take the argument as path
+ 
 else:
     path = sys.argv[1]
 
+
+# Open the file in read mode
+
 f = open(path, "r")
+
+# Read from file
+
 data = f.read()
+
+# Close the file
+
 f.close()
 
+# Split contents of the file into a list of words
+
 l = data.split()
+
+# Print size of the list
+
 print("\nTotal word count :", len(l))
